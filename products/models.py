1a812a38dc184a29de8fe81f07911877b09816ad
Thiago Rodrigues <thiago.rds@outlook.com.br>
2023-03-06 13:01:27 -0400

    first commit

--- products/models.py
+++ products/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
     description = models.CharField(max_length=300, null=True)
     price = models.FloatField()
-    quantity = models.IntegerField()
-    category = models.CharField(max_length=100)
+    stock = models.IntegerField()
     available = models.BooleanField(default=True)
 
-    user = models.ManyToManyField("users.User", related_name="product")
+    # user = models.ManyToManyField("users.User", related_name="product")
--- shopping_carts/models.py
+++ shopping_carts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class ShoppingCarts(models.Model):
+    quantity = models.IntegerField()
+    amount_payable = models.DecimalField()
     user = models.OneToOneField(
         "users.User",
         on_delete=models.CASCADE,
