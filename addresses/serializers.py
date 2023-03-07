9e077820ff29c3dc856823d92acbd09737eade62
Thiago Rodrigues <thiago.rds@outlook.com.br>
2023-03-07 09:42:20 -0400

    feat: add exceptions

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import Address
+from rest_framework.exceptions import ValidationError
 
 
 class AddressSerializer(serializers.ModelSerializer):
     def create(self, validated_data: dict) -> Address:
-        validated_data["user"] = self.context["request"].user
+        user = self.context["request"].user
+        if Address.objects.filter(user=user, **validated_data).exists():
+            raise ValidationError(
+                {
+                    "message": "Já existe um endereço com as mesmas informações para este usuário."
+                }
+            )
+        validated_data["user"] = user
         return Address.objects.create(**validated_data)
 
     def update(self, instance: Address, validated_data: dict) -> Address:
