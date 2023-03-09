8166e6108a327d49989b4007c2ca3b08a0bf416a
unknown <mickelbarros@yahoo.com>
2023-03-09 10:44:49 -0300

    fix: deploy

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from users.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    img = models.CharField(max_length=200)
+    img = models.CharField(max_length=200, default="")
     description = models.CharField(max_length=600)
     category = models.CharField(max_length=100)
     description = models.CharField(max_length=300, null=True)
