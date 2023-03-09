86aa57378fa24154e858410698f285ccbfc03f7c
unknown <mickelbarros@yahoo.com>
2023-03-09 11:33:33 -0300

    fix: deploy 1.2

--- _kenzie_commerce/settings.py
+++ _kenzie_commerce/settings.py
@@ -103,6 +103,10 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": 5432,
+    },
+    "other_name":{
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 DATABASE_URL = os.getenv('DATABASE_URL')
