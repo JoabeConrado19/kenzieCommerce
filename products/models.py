182791426c11438aa1dfb1ff3ba6fd1273584b32
JoabeConrado19 <joabe.conrado19@gmail.com>
2023-03-10 12:01:31 -0400

    Fix: Max_Length

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=300)
     img = models.CharField(max_length=200, blank=True, null=True)
     category = models.CharField(max_length=100)
-    description = models.CharField(null=True)
+    description = models.CharField(max_length=1000, null=True)
     price = models.FloatField()
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
